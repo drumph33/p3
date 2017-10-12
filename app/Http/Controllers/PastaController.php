731cac51227ceda7e02c3a8cab25843921489746
Dylan Rumph <dprumph@gmail.com>
2017-10-11 19:23:28 -0500

    added placeholder routes and controller

---
+++ app/Http/Controllers/PastaController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PastaController extends Controller
+{
+    //
+    public function index()
+    {
+        return 'Display the form';
+    }
+    public function results()
+    {
+        return 'Display the cooking instructions';
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PastaController@index');
+Route::post('/directions', 'PastaController@results');
